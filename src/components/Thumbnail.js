0ae1d2feb4d108fd9c90d7fdec357d934132cb80
Steven Munoz <StevenMunoz@innovaGroup.io>
2023-06-23 13:57:23 -0500

    re deploy settings

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -9,7 +9,7 @@ const Thumbnail = ({ movie }) => {
       className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
     >
       <Image
-        src={`https://image.tmdb.org/t/p/w500${ /* obtendremos la ruta del fondo o del poster */
+        src={`https://image.tmdb.org/t/p/w500${ /* obtendremos la ruta del fondo o del poster */ 
           movie.backdrop_path || movie.poster_path
         }`}
         className="rounded-sm object-cover md:rounded"
